bc263d8584dc9543347302adc7678f112bcd92f4
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2025-02-18 11:41:20 -0700

    Add replay functionality to NumberGame for enhanced user experience, continue to correct output formatting

--- NumberGame.java
+++ NumberGame.java
@@ -6,32 +6,42 @@ public class NumberGame {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int numberToGuess = random.nextInt(100) + 1; // Random number between 1 and 100
-        int attempts = 10; // Limit the number of attempts
-        int guess;
+        String playAgain = "yes";
 
-        System.out.println("\nWelcome to Guess the Number!\n");
-        System.out.println("I have selected a number between 1 and 100. Try to guess it!\n");
+        while (playAgain.equalsIgnoreCase("yes")) {
+            int numberToGuess = random.nextInt(100) + 1; // Random number between 1 and 100
+            int attempts = 10; // Limit the number of attempts
+            int guess;
 
-        for (int i = 1; i <= attempts; i++) {
-            System.out.println("Attempt " + i + ": {" + (attempts - i + 1) + " attempts remaining}");
-            System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
+            System.out.println("\nWelcome to Guess the Number!\n");
+            System.out.println("I have selected a number between 1 and 100. Try to guess it!\n");
 
-            if (guess < numberToGuess) {
-                System.out.println("\nToo low! Try again.\n");
-            } else if (guess > numberToGuess) {
-                System.out.println("\nToo high! Try again.\n");
-            } else {
-                System.out.println("\nCongratulations! You guessed the number in " + i + " attempts.\n");
-                break;
+            for (int i = 1; i <= attempts; i++) {
+                System.out.println("Attempt " + i + ": {" + (attempts - i + 1) + " attempts remaining}");
+                System.out.print("Enter your guess: ");
+                guess = scanner.nextInt();
+
+                if (guess < numberToGuess) {
+                    System.out.println("\nToo low! Try again.\n");
+                } else if (guess > numberToGuess) {
+                    System.out.println("\nToo high! Try again.\n");
+                } else {
+                    System.out.println("\nCongratulations! You guessed the number in " + i + " attempts.\n");
+                    break;
+                }
+
+                if (i == attempts) {
+                    System.out.println("\nSorry! You've run out of attempts. The number was: " + numberToGuess + "\n");
+                }
             }
 
-            if (i == attempts) {
-                System.out.println("\nSorry! You've run out of attempts. The number was: " + numberToGuess + "\n");
+            System.out.print("Would you like to play again? (yes/no): ");
+            playAgain = scanner.next();
+
+            if (!playAgain.equalsIgnoreCase("yes")) {
+                System.out.println("\nThank you for playing Guess The Number! Goodbye!");
             }
         }
-
         scanner.close();
     }
 }
